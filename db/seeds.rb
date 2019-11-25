e130a7a97d999117a11da47c99d0c4ff89b96a82
Miguel Prada <mapra99@gmail.com>
2019-11-24 21:56:00 -0500

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Players
+Player.create(name: 'Tato')
+Player.create(name: 'Toto')
+
+# Circuits
+Circuit.create(name: 'Crash Cove')
+Circuit.create(name: 'Mystery Caves')
+Circuit.create(name: 'Sewer Speedway')
+Circuit.create(name: "Roo's Tubes")
+Circuit.create(name: 'Coco Park')
+Circuit.create(name: 'Tiger Temple')
+Circuit.create(name: 'Dingo Canyon')
+Circuit.create(name: "Papu's Pyramid")
+Circuit.create(name: 'Blizzard Bluff')
+Circuit.create(name: 'Polar Pass')
+Circuit.create(name: 'Tiny Arena')
+Circuit.create(name: 'Dragon Mines')
+Circuit.create(name: 'N. Gin Labs')
+Circuit.create(name: 'Cortex Castle')
+Circuit.create(name: 'Hot Air Skyway')
+Circuit.create(name: 'Oxide Station')
+Circuit.create(name: 'Slide Coliseum')
+Circuit.create(name: 'Turbo Track)
